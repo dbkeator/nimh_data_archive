8a19738169d4e5460fd3dc62fc9d50715626432c
Nolan Nichols <nolan.nichols@gmail.com>
2015-03-23 23:51:19 -0700

    add: data structure object

--- datadictionary.py
+++ datadictionary.py
@@ -18,15 +18,37 @@ class DataDictionary(object):
         self.url = url
         self.request = requests.get(self.url)
         self.json = self.request.json()
-        self.name = self.json['name']
-        self.version = self.json['version']
-        self.description = self.json['description']
-        self.operations = self.json['operations']
+        self.name = self.json.get('name')
+        self.version = self.json.get('version')
+        self.description = self.json.get('description')
+        self.operations = self.json.get('operations')
 
     def __repr__(self):
-        return self.name
+        return "{0}:{1}".format(self.__class__, self.name)
+
+    def get_data_sructures(self):
+        ds_url = '{0}/datastructure'.format(self.url)
+        request = requests.get(ds_url)
+        data = request.json()
+        result = dict()
+        for ds in data:
+            result.update({ds.get('shortName'): DataStructure(ds)})
+        return result
 
 
+class DataStructure(object):
+    def __init__(self, data):
+        self.category = data.get('category')
+        self.data_type = data.get('dataType')
+        self.ndar_url = data.get('ndarURL')
+        self.publish_date = data.get('publishDate')
+        self.short_name = data.get('shortName')
+        self.source = data.get('source')
+        self.status = data.get('status')
+        self.title = data.get('title')
+
+    def __repr__(self):
+        return "{0}:{1}".format(self.__class__, self.short_name)
 
 def main(args=None):
     pass
