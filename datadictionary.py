d8bd9bb6003f146647c3bf9054dabd59ad18df52
Nolan Nichols <nolan.nichols@gmail.com>
2016-06-15 08:32:25 -0400

    doc: cleanup add comments

--- datadictionary.py
+++ datadictionary.py
@@ -9,45 +9,38 @@ Example:
 __author__ = 'Nolan Nichols <https://orcid.org/0000-0003-1099-3328>'
 
 import os
-import json
 import sys
+import json
+import logging
 
 import requests
 
 
-class Connection(object):
+class DataDictionary(object):
+    """Creates an object to retrieve NDA data dictionary.
+    """
     def __init__(self, url):
-        self.url = url
-        self.request = requests.get(self.url)
-
-    def __repr__(self):
-        return "{0}:{1}".format(self.__class__, self.url)
-
-
-class DataDictionary(Connection):
-    def __init__(self, url):
-        super(DataDictionary, self).__init__(url)
+        self.base_url = url
+        self.datastructures_url = None
+        self.request = requests.get(self.base_url)
         self.json = self.request.json()
         self.name = self.json.get('name')
         self.version = self.json.get('version')
         self.description = self.json.get('description')
         self.operations = self.json.get('operations')
-        self.data_structures = None
-
-    def __repr__(self):
-        return "{0}:{1}".format(self.__class__, self.name)
+        self.data_structures = dict()
 
     def get_data_sructures(self):
-        ds_url = '{0}/datastructure'.format(self.url)
-        request = requests.get(ds_url)
+        self.datastructures_url = '{0}/datastructure'.format(self.base_url)
+        request = requests.get(self.datastructures_url)
         data = request.json()
-        result = dict()
         for ds in data:
-            result.update({ds.get('shortName'): DataStructure(ds, self.url)})
-        self.data_structures = result
+            self.data_structures.update({
+                ds.get('shortName'): DataStructure(ds, self.base_url)})
 
 
 class DataStructure(object):
+    """Parses a Data Structure and corresponding data elements."""
     def __init__(self, data, url):
         self.category = data.get('category')
         self.data_type = data.get('dataType')
@@ -57,18 +50,16 @@ class DataStructure(object):
         self.source = data.get('source')
         self.status = data.get('status')
         self.title = data.get('title')
-        self.json = None
+        self.json = dict()
         self.url = url
         self.data = data
         self.data_elements = None
 
-    def __repr__(self):
-        return "{0}:{1}".format(self.__class__, self.short_name)
-
     def get_data_elements(self):
+        """Parses the data elements into a well-structured dictionary."""
         ds_url = '{0}/datastructure/{1}'.format(self.url, self.short_name)
         request = requests.get(ds_url)
-        self.json = request.json()
+        self.json.update(request.json())
         result = dict()
         for de in self.json.get('dataElements'):
             result.update({de.get('name'): DataElement(de)})
@@ -76,6 +67,8 @@ class DataStructure(object):
 
 
 class DataElement(object):
+    """Parses each data element including semi-structured content embedded as
+    json strings."""
     def __init__(self, data):
         self.data = data
         self.required = data.get('required')
@@ -91,6 +84,8 @@ class DataElement(object):
             self.value_range = self.parse_value_range()
 
     def parse_notes(self):
+        """Parses the notes field, attempting to clean up coded values by
+        adding a new "valueset" key to the dictionary."""
         notes = self.data.get('notes')
         codelist = notes.split(";")
         valueset = list()
@@ -109,9 +104,6 @@ class DataElement(object):
 
     def parse_value_range(self):
         return self.data.get('valueRange')
-
-    def __repr__(self):
-        return "{0}:{1}".format(self.__class__, self.name)
 
 
 def main(args=None):
